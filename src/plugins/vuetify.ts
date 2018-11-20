c1537370100fd0b74aa634dce593b0cf045d2861
Jacob Greenfield <jacob@jacobgreenfield.me>
2018-11-19 19:02:00 -0500

    Vuetify à la carte

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,8 @@
 import Vue from "vue";
-import Vuetify from "vuetify";
+import Vuetify from "vuetify/lib";
 import "vuetify/src/stylus/app.styl";
+
+// https://github.com/vuetifyjs/vue-cli-plugin-vuetify/issues/43#issuecomment-435088739
 
 Vue.use(Vuetify, {
   iconfont: "md"
